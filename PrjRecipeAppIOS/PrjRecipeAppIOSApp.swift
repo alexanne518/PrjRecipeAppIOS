96676e42e7c2c071743d784104a6373e8d58a676
Alexanne <alexannefortin18@gmail.com>
2025-11-01 20:55:49 -0400

    Initial Commit

---
+++ PrjRecipeAppIOS/PrjRecipeAppIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrjRecipeAppIOSApp.swift
+//  PrjRecipeAppIOS
+//
+//  Created by Macbook on 2025-11-01.
+//
+
+import SwiftUI
+
+@main
+struct PrjRecipeAppIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
